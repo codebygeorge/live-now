70014c8291aec7844c19edce567c050c607f1047
George Ghazaryan <code.by.george@gmail.com>
2020-04-05 18:38:33 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
-import React, {Component} from 'react';
-import {withRouter} from 'react-router-dom';
-import {Route} from 'react-router-dom';
-import {Switch, Redirect} from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import './scss/style.scss';
-
-import Header from "./components/Header";
-import Landing from "./pages/Landing";
-// import DemoScene from './components/Structural';
-import Item from './components/Item';
-// import Footer from "./components/Footer";
-// import NotFound from "./views/NotFound";v
-
-class App extends Component {
-
-    render() {
-        return (
-            <div className="App">
-                <Header/>
-
-                <main className='main'>
-                    <Switch>
-                        {/*User*/}
-                        {/*<Route exact path="/" component={Landing}/>*/}
-                        <Route exact path="/item" component={Item}/>
-                        {/*<Route exact path="/structural" component={DemoScene}/>*/}
-                        {/*<Route exact path="/notfound" component={NotFound}/>*/}
-
-                        {/*<Redirect to="/"/>*/}
-                        <Redirect to="/item" component={Item}/>
-                    </Switch>
-                </main>
-
-                {/*<Footer/>*/}
-
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default withRouter(props => <App {...props}/>);
+export default App;
