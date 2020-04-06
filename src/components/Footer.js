b96c42f6076873d9e483c71fe5b42e0c4a9fab83
George Ghazaryan <code.by.george@gmail.com>
2020-04-06 20:45:50 +0400

    user pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
+import {Link, NavLink} from 'react-router-dom';
 
 class Footer extends Component {
 
     render() {
         return (
             <footer>
-                <p className="copyright"><small>Design and build by <b>George Ghazaryan</b>, <b>Hayk Aleksanyan</b>, <b>Hayk Ghazaryan</b></small></p>
+                <div className='footer'>
+                    <ul className='footer-menu'>
+                        <li>
+                            <NavLink exact activeClassName="active" to="/how-it-works">
+                                Help
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact activeClassName="active" to="/how-it-works">
+                                Privacy
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact activeClassName="active" to="/how-it-works">
+                                Terms
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact activeClassName="active" to="/how-it-works">
+                                Contact LiveNow
+                            </NavLink>
+                        </li>
+                    </ul>
+
+                    <p className="copyright">
+                        {/*<small>Design and build by <b>George Ghazaryan</b>, <b>Hayk Aleksanyan</b>, <b>Hayk Ghazaryan</b></small>*/}
+                        <small>© 2020 LiveNow, Inc.</small>
+                    </p>
+                </div>
             </footer>
         )
     }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,87 @@
-import React, {Component} from 'react';
-import {AppBar, Toolbar} from '@material-ui/core';
+import React from 'react';
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
-class Header extends Component {
+const menuItems = [
+    {
+        title: "Profile",
+        icon: "user-circle"
+    },
+    {
+        title: "Category",
+        icon: "cubes"
+    },
+    {
+        title: "My Followers",
+        icon: "users"
+    },
+    // {
+    //     title: "Profile",
+    //     icon: <FontAwesomeIcon icon="bars"/>
+    // }
+];
 
-    render() {
-        return (
-            <header>
-                <AppBar position="static">
-                    <Toolbar>
-                        <h1>Live Now</h1>
-                    </Toolbar>
-                </AppBar>
-            </header>
-        )
-    }
+export default function Header(props) {
+    const classes = props.classes;
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <>
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <h1>Live Now</h1>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                className={
+                    `${
+                        clsx(classes.drawer, {
+                            [classes.drawerOpen]: open,
+                            [classes.drawerClose]: !open,
+                        })
+                        } user-menu`
+                }
+                classes={{
+                    paper: clsx({
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open,
+                    }),
+                }}
+            >
+                <Toolbar/>
+                <div className={`${classes.toolbar} toggle-button`}>
+                    <IconButton onClick={handleDrawerToggle}>
+                        <FontAwesomeIcon icon={open ? 'angle-left' : 'angle-right'}/>
+                    </IconButton>
+                </div>
+                <Divider/>
+                <List>
+                    {menuItems.map((item, index) => (
+                        <ListItem button key={index}>
+                            <ListItemIcon>
+                                <FontAwesomeIcon className='menu-icons' icon={item.icon}/>
+                            </ListItemIcon>
+                            <ListItemText>
+                                <p className='ffM'>{item.title}</p>
+                            </ListItemText>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+        </>
+    );
 }
-
-export default Header;
